69be52d04936947853edfa9a1350c1ff73699c62
JamesChen28 <james5520042000@gmail.com>
2021-10-24 17:26:54 +0800

    update py script 20211024

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
+
 # Python_SNA
+User fill your data in **Network_List_v1.0.xlsx**, and run **python_SNA.py**. It will produce SNA plot and centrality data.
+
+## Ver 1.0
+Network_List_v1.0.xlsx
+
+### Data Description
+* **Client**: Fill client name
+* **Project**: Fill project name
+* **Client Project**: Concatenate key for computing, auto-generating by excel, **DON'T FILL** data
+* **Name**: Fill member name
+* **Group**: Define member group
+* **Unique-Gkey**: Concatenate key for computing, auto-generating by excel, **DON'T FILL** data
+
+### Output Description
+Output data will be produced in **OUTPUT** directory.
+* **pdf**: SNA plot
+* **excel**: centrality data
+
--- python_SNA.py
+++ python_SNA.py
@@ -16,8 +16,9 @@ import matplotlib.pyplot as plt
 path = r'D:\James\Desktop\Coding\Python\network'
 os.chdir(path)
 
-data = pd.read_excel('Network_List_v1.0.xlsx', sheet_name='Project_Data')
-
+filename = 'Network_List_v1.0'
+data = pd.read_excel(filename + '.xlsx', sheet_name = 'Project_Data')
+data = data.dropna()
 
 
 #### for Client Project ####
@@ -33,32 +34,33 @@ clean = pd.DataFrame(clean[pd.isna(clean.Name_x) == False]).reset_index()
 # clean.to_excel('./clean_Dev.xlsx', sheet_name = 'clean')
 
 #### graph ####
-G = nx.MultiGraph() #raph() #MultiGraph()
+G = nx.DiGraph() #Graph() #MultiGraph()
 for i in range(clean.shape[0]):
     G.add_edges_from([(clean['Name_x'][i], clean['Name_y'][i])], Label = clean['Unique-Gkey'][i]) # have to change
     G.add_nodes_from([clean['Name_x'][i]], TYPE = 'daimond')
 
-print(G.edges(data=True))
+# print(G.edges(data=True))
+
 #### Cal ####
-degree = pd.DataFrame(pd.Series(nx.algorithms.centrality.degree_centrality(G))).rename(columns={0: "Degree"})
-degree = degree.sort_values(by=['Degree'], ascending=False)
+degree = pd.DataFrame(pd.Series(nx.algorithms.centrality.degree_centrality(G))).rename(columns = {0: "Degree"})
+degree = degree.sort_values(by = ['Degree'], ascending = False)
 # between = nx.algorithms.centrality.edge_betweenness_centrality(G)
-# close = nx.algorithms.centrality.closeness_centrality(G)
+# between = nx.algorithms.centrality.betweenness_centrality(G)
+between = pd.DataFrame(pd.Series(nx.algorithms.centrality.betweenness_centrality(G))).rename(columns = {0: "Betweenness"})
+between = between.sort_values(by = ['Betweenness'], ascending = False)
+close = pd.DataFrame(pd.Series(nx.algorithms.centrality.closeness_centrality(G))).rename(columns = {0: "Closeness"})
+close = close.sort_values(by = ['Closeness'], ascending = False)
+
 # print(degree)
+
+center = degree.merge(between, how = 'left', left_index = True, right_index = True).merge(close, how = 'left', left_index = True, right_index = True)
+
+
 #### Plot ####
-pos = nx.spring_layout(G, k=0.8)
-edge_labels = {(edge[0], edge[1]): edge[2]['Label'] for edge in G.edges(data=True)}
-nx.draw(G,
-        pos,
-        with_labels=True,
-        node_size=1,
-        font_size=7,
-        width=0.1)
-nx.draw_networkx_edge_labels(G,
-                             pos,
-                             edge_labels = edge_labels,
-                             font_size=3,
-                             label_pos=0.6)
+pos = nx.spring_layout(G, k = 0.8)
+edge_labels = {(edge[0], edge[1]): edge[2]['Label'] for edge in G.edges(data = True)}
+nx.draw(G, pos, with_labels = True, node_size = 1, font_size = 10, width = 0.1)
+nx.draw_networkx_edge_labels(G, pos, edge_labels = edge_labels, font_size = 5, label_pos = 0.6)
 
 plt.show()
 
@@ -66,7 +68,7 @@ plt.show()
 if not os.path.exists('Output'):
     os.makedirs('Output')
 
-nx.write_graphml(G,"Output/Network_List_v1.0.graphml")
-plt.savefig("Output/Plot_Network_List_v1.0.pdf")
-degree.to_excel('Output/Degree_Network_List_v1.0.xlsx', sheet_name = 'degree')
+# nx.write_graphml(G,'Output/' + filename+ '.graphml')
+plt.savefig('Output/Plot_' + filename+ '.pdf')
+center.to_excel('Output/Centrality_' + filename+ '.xlsx', sheet_name = 'centrality')
 
